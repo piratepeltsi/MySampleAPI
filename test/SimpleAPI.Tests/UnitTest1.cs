dbccd27845a1056baaa7b7b5c8344506dafe9985
Jukka Peltonen <jtpeltonen@hotmail.com>
2021-12-12 23:49:24 +0200

    Commit 4

--- test/SimpleAPI.Tests/UnitTest1.cs
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -5,13 +5,7 @@ namespace SimpleAPI.Tests;
 
 public class UnitTest1
 {
-    ValuesController controller = new ValuesController();
-    [Fact]
-    public void GetReturnsCorrectNumber()
-    {
-        var returnValue = controller.Get(1);
-        Assert.Equal("Les Jackson", returnValue.Value);
-    }
+
 
     [Fact]
     public void Test1()
